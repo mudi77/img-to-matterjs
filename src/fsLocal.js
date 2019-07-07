5a9cb56b492a7b19c312f8e84319e6d6e5a42403
Igor Collak <igor.collak@fpt.sk>
2019-07-07 04:18:59 +0200

    change path resolve

--- src/fsLocal.js
+++ src/fsLocal.js
@@ -20,13 +20,11 @@ let fsLocal = {
             if(!path || path == "") reject("Wrong path to images directory !");
 
             try{
-                fs.readdirSync( pth.normalize(path) ).map((imgName) => {                   
+                fs.readdirSync( pth.resolve( path ) ).map((imgName) => {                         
                     let url = `${path}/${imgName}`;
                     let format = imgName.split('.')[1];
 
-                    if(!fs.lstatSync(url).isDirectory() && format == "png" || format == "jpg"){      //SYNCHRONOUS READ
-
-                        console.log("load img :: ", imgName + " format " + format);
+                    if(!fs.lstatSync(url).isDirectory() && format == "png" || format == "jpg"){      //SYNCHRONOUS READ                      
 
                         imgList.push({
                             "name": imgName.split('.')[0],
@@ -46,7 +44,7 @@ let fsLocal = {
         let outputJsonFile = undefined;
 
         try{
-            outputJsonFile = fs.readFileSync( pth.normalize(path), 'utf8');
+            outputJsonFile = fs.readFileSync( pth.resolve(path), 'utf8');
         }catch (err){
             //return new Error();
         }
@@ -62,7 +60,7 @@ let fsLocal = {
         
             if(!path || path == "") return reject("Wrong path to output save directory !");
 
-            path = pth.normalize(path);
+            path = pth.resolve(path);
             
             if(!splitJson){
                 fs.writeFile(`${path}/shapesFile.json`, JSON.stringify(data, null, 2), () => {}, function(err) {
